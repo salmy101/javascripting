f3136400080f2e0d82b02ccdd123ca693250aeeb
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-11 19:00:59 +0000

    Initial comment w/ all my work so far, added all javascripting files

---
+++ array-filtering.js
@@ -0,0 +1,8 @@
+const number = [1,2,3,4,5,6,7,8,9,10]
+const filtered = number.filter(function evenNumbers (number) {  
+  return number % 2 === 0  
+}  
+)
+console.log(filtered)
+
+console.log(16%2)
